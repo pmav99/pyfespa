9a9206308d8a8ddf1e5f84ae7a07441ff38690c6
pmav99@gmail.com <pmav99@gmail.com@b7be6da6-17dc-11df-98df-676f987e6d01>
2010-02-19 11:53:11 +0000

    Σημαντικές αλλαγές. 1. Πλεόν σαν input ζητείται το όνομα του αρχείου *.tek 2. Το πρόγραμμα τρέχει για όλα τα αρχεία dxf που το όνομα τους ξεκινάει με το όνομα του αρχείου tek (τα αρχεία δλδ που παράχθηκαν από το FESPA) και βρίσκονται στον current working directory

--- trunk/pyfespa.py
+++ trunk/pyfespa.py
@@ -41,7 +41,7 @@ def get_MTEXT(c5,c8,c10,c20,c40,c41,c71,c72,c1,c7,c50,newlines):
     """
     s=r'0\nMTEXT\n5\n%s\n100\nAcDbEntity\n8\n%s\n100\nAcDbMText\n10\n%s\n20\n%s\n40\n%s\n41\n%s\n71\n%s\n72\n%s\n1\n%s\n7\n%s\n50\n%s\n'%(c5,c8,c10,c20,c40,c41,c71,c72,c1,c7,c50)
     if newlines:
-            s=s.replace(r'\n','\n').strip()+'\n'
+        s=s.replace(r'\n','\n').strip()+'\n'
     return s
     
 def get_MTEXT_regex(layer):
@@ -72,7 +72,7 @@ def fix_slabs(f, prefix_layer, element_layer):
         old=i[0]
         new=get_MTEXT(handle,layer,x0,y0,h,w,1,1,text,layer,angle,True)
         f=f.replace(old,new)
-        print ("replacing : ",text)
+        #print ("replacing : ",text)
     return f
 
 def fix_slabs2(f,element_layer):
@@ -95,7 +95,7 @@ def fix_slabs2(f,element_layer):
         old=i[0]
         new=get_MTEXT(handle,layer,x0,y0,h,w,1,1,text,layer,angle,True)
         f=f.replace(old,new)
-        print ("replacing : ",text)
+        #print ("replacing : ",text)
     return f
 def fix_beams(f, prefix_layer, element_layer):
     """
@@ -118,7 +118,7 @@ def fix_beams(f, prefix_layer, element_layer):
         old=i[0]
         new=get_MTEXT(handle,layer,x0,y0,h,w,1,1,text,layer,angle,True)
         f=f.replace(old,new)
-        print ("replacing : ",text)
+        #print ("replacing : ",text)
     return f
 
 def fix_columns(f, prefix_layer, element_layer):
@@ -141,33 +141,13 @@ def fix_columns(f, prefix_layer, element_layer):
         old=i[0]
         new=get_MTEXT(handle,layer,x0,y0,h,w,1,1,text,layer,angle,True)
         f=f.replace(old,new)
-        print ("replacing : ",text)      
+        #print ("replacing : ",text)      
     return f
 
-if __name__ == "__main__":
-    s = u"""
-----------------------------------------------------------------------------
-|Το πρόγραμμα αυτό επεξεργάζεται τα αρχεία dxf που παράγονται από το FESPΑ.|
-|                                                                          |
-|Στην εισαγωγή του αρχείου εισόδου ΔΕΝ πρέπει να γράφετε η επέκταση        |
-|(extension) του αρχείου. Π.χ. "filename" και όχι "filename.dxf".          |
-|                                                                          |
-|Το όνομα του αρχείου εξόδου είναι ίδιο με αυτό του αρχείου εισόδου, με την|
-|προσθήκη του "_FIXED" στο τέλος του ονόματος.                             |
-----------------------------------------------------------------------------
+def fixing(f):
+    u"""
+    Η συνάρτηση αυτή καλεί τις υπόλοιπες συναρτήσεις που διορθώνουν τα αρχεία dxf
     """
-    print(s)            # Printing welcoming string
-    
-    #Opening reading and closing input file
-    base_name=raw_input('Name of the file  : ') 
-    ifilename=base_name + ".dxf"
-    try:
-        ifile=open(ifilename,'r')
-    except:
-        raise 'cannot open file'
-    f=ifile.read()
-    ifile.close()
-
     # merging the layers
     f=fix_beams(f, 'beam_prefix_name_beton', 'beam_name_beton_upperstruct')
     f=fix_beams(f, 'beam_prefix_name_beton', 'beam_name_beton_found_shear_wal')
@@ -178,13 +158,60 @@ if __name__ == "__main__":
     
     # replace the letter Φ with the diameter sign
     f=f.replace('Φ'.decode('utf-8').encode('windows-1253'),'%%C')
+    
+    return f
 
-    # opening, writing and closing output file
-    ofilename=base_name+"_FIXED.dxf"
-    try:
-        ofile=open(ofilename,'w')
-    except:
-        raise 'cannot open file'
-    ofile.write(f)
-    ofile.close()
+if __name__ == "__main__":
+    s = u"""
+----------------------------------------------------------------------------
+|Το πρόγραμμα αυτό επεξεργάζεται τα αρχεία dxf που παράγονται από το FESPΑ.|
+|                                                                          |
+|Στην εισαγωγή του αρχείου εισόδου ΔΕΝ πρέπει να γράφεται η επέκταση       |
+|(extension) του αρχείου. Π.χ. "filename" και όχι "filename.tek".          |
+|                                                                          |
+|Το όνομα του αρχείου εξόδου είναι ίδιο με αυτό του αρχείου εισόδου, με την|
+|προσθήκη του "_FIXED" στο τέλος του ονόματος.                             |
+|                                                                          |
+|Έτσι τα αρχεία που παράχθηκαν από το "filename.tek", δηλαδή τα :          |
+|"filename_Or##.dxf" θα γίνουν "filename_Or##_FIXED.dxf"                   |
+----------------------------------------------------------------------------
+    """
+    print(s)            # Printing welcoming string
+    
+    # Getting the path of the current directory
+    path = os.path.abspath('')
+
+    #Getting a list with all the files in the directory
+    directory_files = os.listdir(path)
+
+    #Getting the input file base name
+    base_name=raw_input("Give the name of the *.tek file : ")
+
+    for file in directory_files:
+        if file.startswith(base_name) and file.endswith(".dxf"):
+            #Opening reading and closing input file
+            try:
+                ifile=open(file,'r')
+            except:
+                raise 'cannot open file'
+            f=ifile.read()
+            ifile.close()
+
+            # fix dxf file
+            f=fixing(f)
+
+            #setting the output file name
+            ofilename=file.replace(".dxf","_FIXED.dxf")
+
+            # opening, writing and closing output file
+            try:
+                ofile=open(ofilename,'w')
+            except:
+                raise 'cannot open file'
+            ofile.write(f)
+            ofile.close()
+
+            print("The new file is : ",ofilename)
+        else:
+            print("The file is not a dxf, or it doesn't start with the specified name.")          
     raw_input('press <enter> to exit...')
