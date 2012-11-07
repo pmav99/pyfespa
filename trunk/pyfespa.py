26f37690947607f403909cd5c2adae28dc78369d
pmav99@gmail.com <pmav99@gmail.com@b7be6da6-17dc-11df-98df-676f987e6d01>
2012-11-07 10:06:51 +0000

    [Major] Make Python3 compatible and improve user interface.

--- trunk/pyfespa.py
+++ trunk/pyfespa.py
@@ -31,15 +31,39 @@
 # i[9]  = Text string
 # i[10] = Rotation Angle
 
+from __future__ import division
 from __future__ import print_function
+from __future__ import with_statement
 
 import re
-import os
+import glob
+import codecs
 
 # python3 compatibility
 import sys
 if sys.version_info.major >= 3:
     raw_input = input
+
+
+WELCOME_MESSAGE = """
+|------------------------------------------------------------------------------|
+|Το πρόγραμμα αυτό επεξεργάζεται τα αρχεία dxf που παράγονται από το FESPΑ.    |
+|                                                                              |
+|Σαν input δίνεται το όνομα του αρχείου *.tek ΧΩΡΙΣ την επέκταση!!!            |
+|Π.χ. "filename" και όχι "filename.tek".                                       |
+|                                                                              |
+|Το όνομα του αρχείου εξόδου είναι ίδιο με αυτό του αρχείου εισόδου, με την    |
+|προσθήκη του "_FIXED" στο τέλος του ονόματος.                                 |
+|                                                                              |
+|Έτσι τα αρχεία που παράχθηκαν από το "filename.tek", δηλαδή τα :              |
+|"filename_Or##.dxf" θα γίνουν "filename_Or##_FIXED.dxf"                       |
+|------------------------------------------------------------------------------|
+"""
+
+# Output format
+OUTPUT = "|{0:38s}|{1:39s}|"
+RULE = OUTPUT.format("-" * 38, "-" * 39)
+
 
 def get_MTEXT(c5, c8, c10, c20, c40, c41, c71, c72, c1, c7, c50, newlines):
     """
@@ -48,7 +72,7 @@ def get_MTEXT(c5, c8, c10, c20, c40, c41, c71, c72, c1, c7, c50, newlines):
     s = r'0\nMTEXT\n5\n%s\n100\nAcDbEntity\n8\n%s\n100\nAcDbMText\n10\n%s\n20\n%s\n40\n%s\n41\n%s\n71\n%s\n72\n%s\n1\n%s\n7\n%s\n50\n%s\n' % \
             (c5, c8, c10, c20, c40, c41, c71, c72, c1, c7, c50)
     if newlines:
-        s=s.replace(r'\n','\n').strip()+'\n'
+        s = s.replace(r'\n','\n').strip()+'\n'
     return s
 
 def get_MTEXT_regex(layer):
@@ -56,18 +80,18 @@ def get_MTEXT_regex(layer):
     Δημιουργεί ένα regex το οποίο ταιριάζει με MTEXT's που ανήκουν
     σε ένα δεδομένο layer.
     """
-    c=r'(.*)'
-    s=get_MTEXT(c, c, c, c, c, c, c, c, r'\{\\f\|b0\|i0\;(.*)\}', layer, c, False)
+    c = r'(.*)'
+    s = get_MTEXT(c, c, c, c, c, c, c, c, r'\{\\f\|b0\|i0\;(.*)\}', layer, c, False)
     return s
 
-def fix_slabs(f, prefix_layer, element_layer):
+def fix_slabs(dxf, prefix_layer, element_layer):
     """
     replace slab text: Π+8 -> Π8
     """
-    s1=get_MTEXT_regex(element_layer)
-    s2=get_MTEXT_regex(prefix_layer)
-    pat=re.compile('('+s1+s2+')')
-    matches=pat.findall(f)
+    s1 = get_MTEXT_regex(element_layer)
+    s2 = get_MTEXT_regex(prefix_layer)
+    pat = re.compile('('+s1+s2+')')
+    matches = pat.findall(dxf)
     for i in matches:
         handle   = i[11]
         x0       = i[13]
@@ -77,20 +101,20 @@ def fix_slabs(f, prefix_layer, element_layer):
         text     = i[19] + i[9]
         layer    = element_layer
         angle    = i[10]
-        old=i[0]
-        new=get_MTEXT(handle, layer, x0, y0, h, w, 1, 1, text, layer, angle, True)
-        f=f.replace(old, new)
+        old = i[0]
+        new = get_MTEXT(handle, layer, x0, y0, h, w, 1, 1, text, layer, angle, True)
+        dxf = dxf.replace(old, new)
         #print ("replacing : ", text)
-    return f
+    return dxf
 
-def fix_slabs2(f, element_layer):
+def fix_slabs2(dxf, element_layer):
     """
     replace slab text: h= + 15 -> h=15
     """
-    s1=get_MTEXT_regex(element_layer)
-    s2=get_MTEXT_regex(element_layer)
-    pat=re.compile('('+s1+s2+')')
-    matches=pat.findall(f)
+    s1 = get_MTEXT_regex(element_layer)
+    s2 = get_MTEXT_regex(element_layer)
+    pat = re.compile('('+s1+s2+')')
+    matches = pat.findall(dxf)
     for i in matches:
         handle   = i[1]
         x0       = i[3]
@@ -100,21 +124,21 @@ def fix_slabs2(f, element_layer):
         text     = i[9]+i[19]
         layer    = element_layer
         angle    = i[10]
-        old=i[0]
-        new=get_MTEXT(handle, layer, x0, y0, h, w, 1, 1, text, layer, angle, True)
-        f=f.replace(old, new)
+        old = i[0]
+        new = get_MTEXT(handle, layer, x0, y0, h, w, 1, 1, text, layer, angle, True)
+        dxf = dxf.replace(old, new)
         #print ("replacing : ", text)
-    return f
+    return dxf
 
-def fix_beams(f, prefix_layer, element_layer):
+def fix_beams(dxf, prefix_layer, element_layer):
     """
     replace beam text: Δ+9+30/60 -> Δ9 30/60
     """
-    s1=get_MTEXT_regex(prefix_layer)
-    s2=get_MTEXT_regex(element_layer)
-    s3=get_MTEXT_regex(element_layer)
-    pat=re.compile('('+s1+s2+s3+')')
-    matches=pat.findall(f)
+    s1 = get_MTEXT_regex(prefix_layer)
+    s2 = get_MTEXT_regex(element_layer)
+    s3 = get_MTEXT_regex(element_layer)
+    pat = re.compile('('+s1+s2+s3+')')
+    matches = pat.findall(dxf)
     for i in matches:
         handle   = i[1]
         x0       = i[3]
@@ -124,20 +148,20 @@ def fix_beams(f, prefix_layer, element_layer):
         text     = i[9] + i[19] + ' ' + i[29].strip()
         layer    = element_layer
         angle    = i[10]
-        old=i[0]
-        new=get_MTEXT(handle, layer, x0, y0, h, w, 1, 1, text, layer, angle, True)
-        f=f.replace(old, new)
+        old = i[0]
+        new = get_MTEXT(handle, layer, x0, y0, h, w, 1, 1, text, layer, angle, True)
+        dxf = dxf.replace(old, new)
         #print ("replacing : ", text)
-    return f
+    return dxf
 
-def fix_columns(f, prefix_layer, element_layer):
+def fix_columns(dxf, prefix_layer, element_layer):
     """
     replace column text: K+8 -> K8
     """
-    s1=get_MTEXT_regex(prefix_layer)
-    s2=get_MTEXT_regex(element_layer)
-    pat=re.compile('('+s1+s2+')')
-    matches=pat.findall(f)
+    s1 = get_MTEXT_regex(prefix_layer)
+    s2 = get_MTEXT_regex(element_layer)
+    pat = re.compile('('+s1+s2+')')
+    matches = pat.findall(dxf)
     for i in matches:
         handle   = i[1]
         x0       = i[3]
@@ -147,80 +171,84 @@ def fix_columns(f, prefix_layer, element_layer):
         text     = i[9] + i[19]
         layer    = element_layer
         angle    = i[10]
-        old=i[0]
-        new=get_MTEXT(handle, layer, x0, y0, h, w, 1, 1, text, layer, angle, True)
-        f=f.replace(old, new)
+        old = i[0]
+        new = get_MTEXT(handle, layer, x0, y0, h, w, 1, 1, text, layer, angle, True)
+        dxf = dxf.replace(old, new)
         #print ("replacing : ", text)
-    return f
+    return dxf
 
-def fixing(f):
-    u"""
+def fix_dxf(dxf):
+    """
     Η συνάρτηση αυτή καλεί τις υπόλοιπες συναρτήσεις που διορθώνουν τα αρχεία dxf
     """
     # merging the layers
-    f=fix_beams(f, 'beam_prefix_name_beton', 'beam_name_beton_upperstruct')
-    f=fix_beams(f, 'beam_prefix_name_beton', 'beam_name_beton_found_shear_wal')
-    f=fix_beams(f, 'beam_prefix_name_beton', 'beam_name_beton_foundation')
-    f=fix_columns(f, 'column_prefix_name_beton', 'column_name_beton')
-    f=fix_slabs(f, 'slab_prefix_name', 'slab_name')
-    f=fix_slabs2(f, 'slab_name')
+    dxf = fix_beams(dxf, 'beam_prefix_name_beton', 'beam_name_beton_upperstruct')
+    dxf = fix_beams(dxf, 'beam_prefix_name_beton', 'beam_name_beton_found_shear_wal')
+    dxf = fix_beams(dxf, 'beam_prefix_name_beton', 'beam_name_beton_foundation')
+    dxf = fix_columns(dxf, 'column_prefix_name_beton', 'column_name_beton')
+    dxf = fix_slabs(dxf, 'slab_prefix_name', 'slab_name')
+    dxf = fix_slabs2(dxf, 'slab_name')
 
     # replace the letter Φ with the diameter sign
-    f=f.replace('Φ'.decode('utf-8').encode('windows-1253'), '%%C')
+    if sys.version_info.major < 3:
+        dxf = dxf.replace('Φ'.decode('utf-8').encode('windows-1253'), '%%C')
+    else:
+        dxf = dxf.replace(u"Φ", "%%C")
 
-    return f
+    return dxf
+
+def main():
+    # get a list of the dxf files in the current folder.
+    dxf_files = glob.glob("*.dxf")
+
+    # Get basename
+    print(WELCOME_MESSAGE)
+    base_name=raw_input("Give the name of the *.tek file : ")
+    print()
+
+    # If there are not any dxf files starting with the given basename,
+    # then exit gracefully.
+    if not any([fname.startswith(base_name) for fname in dxf_files]):
+        print("I couldn't find any dxf file named \"%s*.dxf\".\n" % base_name,
+              "Are you sure you typed the correct name?")
+        return
+
+    # print output header
+    print(RULE)
+    print(OUTPUT.format("Old Filename", "New Filename"))
+    print(RULE)
+
+    # fix the dxf files
+    for fname in dxf_files:
+        # Fix only the files that start with the given basename!
+        if fname.startswith(base_name):
+            with codecs.open(fname, "r") as f:
+                dxf = f.read()
+
+            dxf=fix_dxf(dxf)
+
+            ofname=fname.replace(".dxf", "_FIXED.dxf")
+
+            # Write the output file.
+            # If the file is in use(e.g. open in Autocad) then exit gracefully.
+            try:
+                with codecs.open(ofname, "w") as f:
+                    f.write(dxf)
+            # Actually Python3 raises a PermissionError but it is a subclass of
+            # IOError, so catching IOError works for both Python2 and Python3.
+            except IOError:
+                print("\n\"%s\" appears to be in use.\n" % ofname,
+                      "Please close the application using it and rerun the script.")
+                raw_input('\npress <enter> to exit...')
+                return
+
+            print(OUTPUT.format(fname, ofname))
+
+    print(RULE)
+    raw_input('\nFinished! press <enter> to exit...')
+    return
 
 if __name__ == "__main__":
-    s = u"""
-----------------------------------------------------------------------------
-|Το πρόγραμμα αυτό επεξεργάζεται τα αρχεία dxf που παράγονται από το FESPΑ.|
-|                                                                          |
-|Στην εισαγωγή του αρχείου εισόδου ΔΕΝ πρέπει να γράφεται η επέκταση       |
-|(extension) του αρχείου. Π.χ. "filename" και όχι "filename.tek".          |
-|                                                                          |
-|Το όνομα του αρχείου εξόδου είναι ίδιο με αυτό του αρχείου εισόδου, με την|
-|προσθήκη του "_FIXED" στο τέλος του ονόματος.                             |
-|                                                                          |
-|Έτσι τα αρχεία που παράχθηκαν από το "filename.tek", δηλαδή τα :          |
-|"filename_Or##.dxf" θα γίνουν "filename_Or##_FIXED.dxf"                   |
-----------------------------------------------------------------------------
-    """
-    print(s)            # Printing welcoming string
+    main()
 
-    # Getting the path of the current directory
-    path = os.path.abspath('')
 
-    #Getting a list with all the files in the directory
-    directory_files = os.listdir(path)
-
-    #Getting the input file base name
-    base_name=raw_input("Give the name of the *.tek file : ")
-
-    for file in directory_files:
-        if file.startswith(base_name) and file.endswith(".dxf"):
-            #Opening reading and closing input file
-            try:
-                ifile=open(file, 'r')
-            except:
-                raise 'cannot open file'
-            f=ifile.read()
-            ifile.close()
-
-            # fix dxf file
-            f=fixing(f)
-
-            #setting the output file name
-            ofilename=file.replace(".dxf", "_FIXED.dxf")
-
-            # opening, writing and closing output file
-            try:
-                ofile=open(ofilename, 'w')
-            except:
-                raise 'cannot open file'
-            ofile.write(f)
-            ofile.close()
-
-            print("The new file is : ", ofilename)
-        else:
-            print("The file is not a dxf, or it doesn't start with the specified name.")
-    raw_input('press <enter> to exit...')
